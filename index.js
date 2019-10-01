416ce85f42f866516d5002506761065e3b7409b1
xiekind <ziekind07@gmail.com>
2019-10-01 17:25:41 +0800

    working File

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ app.get('/', function(req,res){
     res.sendFile(__dirname+ '/index.html')
 })
 
-app.use(express.static(__dirname+ 'public'))
+app.use(express.static('public'));
+// app.use(express.static(__dirname+ 'public'));
+
 http.listen(port, function(){
     console.log('Listening on port : ' + port)
 })
