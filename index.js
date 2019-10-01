9eef6ca1637d03ece441dd32027004e0cbd6ab8d
xiekind <ziekind07@gmail.com>
2019-10-01 16:39:09 +0800

    mqtt broker and client

---
+++ index.js
@@ -0,0 +1,13 @@
+var app = require('express')()
+var express = require('express')
+var http = require('http').createServer(app)
+var port = process.env.PORT || 3001
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname+ '/index.html')
+})
+
+app.use(express.static(__dirname+ 'public'))
+http.listen(port, function(){
+    console.log('Listening on port : ' + port)
+})
